9a210b8ac34f117cffe360fb0f867554029eb0e2
Abelrt11 <abel.romerotavio@gmail.com>
2024-09-29 16:01:13 +0100

    Created two methods, one private and the other public, the first one is to check if there is bread of an specific type in the bakery, and the other one is for customer to buy bread

--- src/main/java/es/Miulpgc/Bakery.java
+++ src/main/java/es/Miulpgc/Bakery.java
@@ -25,4 +25,20 @@ public class Bakery {
     public void setBreads_per_type(int[] breads_per_type) {
         Breads_per_type = breads_per_type;
     }
+
+    private boolean is_this_bread_available(String bread) {
+        for (int i = 0; i < Types_of_bread.length; i++) {
+            if (bread.equals(Types_of_bread[i]) && Breads_per_type[i] >= 1) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public String buy_bread(String bread) {
+        if (is_this_bread_available(bread)) {
+            return "Bread bought succesfully";
+        }
+        return "Bread not bought succesfully due to shortage";
+    }
 }
